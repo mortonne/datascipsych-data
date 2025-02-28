3cffaed576a35f608e995f25efc169493a2ccf2d
Neal Morton <mortonne@gmail.com>
2025-02-27 18:02:01 -0600

    feat: quick draft of conversion

--- datasets/2011_Polyn_JExpPsycholLangMemCognit/convert_frdata2table.m
+++ datasets/2011_Polyn_JExpPsycholLangMemCognit/convert_frdata2table.m
@@ -1,3 +1,15 @@
 load PolyEtal11_JEPLMC_data.mat
+
 data.pres.category = data.pres_catnos;
 data.rec.category = data.rec_catnos;
+
+data.pres.period = zeros(size(data.pres_cat));
+data.rec.period = data.rec_period;
+
+data.pres.task = repmat(data.rec_task, 1, size(data.pres_items, 2))
+data.rec.task = repmat(data.rec_task, 1, size(data.rec_items, 2))
+
+tab = frdata2table(data, {'category', 'task', 'period'});
+tab.category(isnan(tab.category)) = 0
+
+writetable(tab, '2011_Polyn.csv')
