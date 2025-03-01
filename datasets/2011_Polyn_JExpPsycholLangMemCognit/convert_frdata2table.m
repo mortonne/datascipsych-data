874d3171bfc423eb4fa5101907c9c23fc708ff27
Neal Morton <mortonne@gmail.com>
2025-02-28 20:33:10 -0600

    feat: period information for study events

--- datasets/2011_Polyn_JExpPsycholLangMemCognit/convert_frdata2table.m
+++ datasets/2011_Polyn_JExpPsycholLangMemCognit/convert_frdata2table.m
@@ -4,6 +4,14 @@ data.pres.category = data.pres_cat;
 data.rec.category = data.rec_cat;
 
 data.pres.period = zeros(size(data.pres_cat));
+for i = 1:size(data.taskcatno, 1)
+    for j = 1:size(data.taskcatno, 2)
+        if ~isnan(data.taskcatno(i, j))
+            cat = data.taskcatno(i, j);
+            data.pres.period(i, data.pres.category(i, :) == cat) = j;
+        end
+    end
+end
 data.rec.period = data.rec_period;
 
 data.pres.task = repmat(data.rec_task, 1, size(data.pres_items, 2));
